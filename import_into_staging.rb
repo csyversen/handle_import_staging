4dc296e7f09808f9505dff1ad29ff84d189b287c
Chris Syversen <christopher.syversen@northwestern.edu>
2013-06-21 10:30:08 -0500

    commented out the URL change line. We don't want to update the URL quite yet!

--- import_into_staging.rb
+++ import_into_staging.rb
@@ -2,7 +2,7 @@ output = File.new("handle_batch_into_staging.txt", "w")
 input = File.open("handles17JUNE2013.txt").read
 input.gsub!(/\r\n?/, "\n") #replace end of line characters
 input.gsub!(/\"/, "") #replace quotes with nothing
-input.gsub!(/https:\/\/rosebud.library.northwestern.edu\/dms\/mastervideo\//,"http://mastervideo-dev.library.northwestern.edu/mastervideo/current/") #replace rosebud domain to mastervideo-dev domain
+#input.gsub!(/https:\/\/rosebud.library.northwestern.edu\/dms\/mastervideo\//,"http://mastervideo-dev.library.northwestern.edu/mastervideo/current/") #replace rosebud domain to mastervideo-dev domain
 input.gsub!(/MEDIA/,"STG") #replace MEDIA to STG
 
 
